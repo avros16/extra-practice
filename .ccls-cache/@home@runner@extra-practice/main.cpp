226d0dfdc5d149702735b78dc794dd286156018a
avros16 <114757828+avros16@users.noreply.github.com>
2024-03-28 16:08:25 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@extra-practice/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+// Q4. Write a C++ program to find the sum of all even numbers between 1 and a given integer input by the user.
+
+// Q5. Implement a C++ program to check whether a given number is prime or not.
+
+// Q6. Develop a C++ program to reverse a given integer number.
+
+int main() {
+  std::cout << "Hello World!\n";
+}
